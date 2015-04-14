c4d9a86ceac5efe8bb117e0159917c112e2f0e4f
chusik <alexx2000@mail.ru>
2015-04-14 08:36:20 +0300

    ADD: Extract support

--- archivefunc.pas
+++ archivefunc.pas
@@ -1,25 +1,29 @@
 unit ArchiveFunc;
 
 {$mode objfpc}{$H+}
+{$I calling.inc}
 
 interface
 
 uses
   Classes, SysUtils, WcxPlugin;
 
-function OpenArchive(var ArchiveData : tOpenArchiveData) : TArcHandle; cdecl;
-function ReadHeader(hArcData : TArcHandle; var HeaderData: THeaderData) : Integer; cdecl;
-function ReadHeaderEx(hArcData : TArcHandle; var HeaderData: THeaderDataEx) : Integer; cdecl;
-function ProcessFile(hArcData : TArcHandle; Operation : Integer; DestPath, DestName : PChar) : Integer; cdecl;
-function CloseArchive(hArcData : TArcHandle) : Integer; cdecl;
+function OpenArchive(var ArchiveData : tOpenArchiveData) : TArcHandle; dcpcall;
+function ReadHeader(hArcData : TArcHandle; var HeaderData: THeaderData) : Integer; dcpcall;
+function ReadHeaderEx(hArcData : TArcHandle; var HeaderData: THeaderDataEx) : Integer; dcpcall;
+function ProcessFile(hArcData : TArcHandle; Operation : Integer; DestPath, DestName : PChar) : Integer; dcpcall;
+function CloseArchive(hArcData : TArcHandle) : Integer; dcpcall;
 
-procedure SetChangeVolProc(hArcData : TArcHandle; pChangeVolProc : PChangeVolProc); cdecl;
-procedure SetProcessDataProc(hArcData : TArcHandle; pProcessDataProc : TProcessDataProc); cdecl;
+procedure SetChangeVolProc(hArcData : TArcHandle; pChangeVolProc : PChangeVolProc); dcpcall;
+procedure SetProcessDataProc(hArcData : TArcHandle; pProcessDataProc : TProcessDataProc); dcpcall;
 
 implementation
 
 uses
-  uArchive;
+  CTypes, uArchive, DCFileAttributes, DCDateTimeUtils, DCOSUtils, Windows;
+
+var
+  ProcessDataProc: TProcessDataProc;
 
 type
   TArchiveHandle = class
@@ -27,7 +31,31 @@ type
     ArchiveEntry: PArchiveEntry;
   end;
 
-function OpenArchive(var ArchiveData: tOpenArchiveData): TArcHandle; cdecl;
+procedure GetFileName(FileName: PAnsiChar; var HeaderData: THeaderDataEx);
+var
+  Index: Integer = 0;
+begin
+  while (FileName[Index] <> #0) do
+  begin
+    if FileName[Index] = '/' then
+      HeaderData.FileName[Index]:= PathDelim
+    else begin
+      HeaderData.FileName[Index]:= FileName[Index];
+    end;
+    Inc(Index);
+  end;
+  HeaderData.FileName[Index]:= #0;
+end;
+
+function GetFileTime(FileTime: PtrInt): PtrInt;
+begin
+  Result:= FileTime;;
+  {$IFDEF MSWINDOWS}
+   Result   := DateTimeToDosFileTime(UnixFileTimeToDateTime(Result));
+  {$ENDIF}
+end;
+
+function OpenArchive(var ArchiveData: tOpenArchiveData): TArcHandle; dcpcall;
 var
   Handle: TArchiveHandle;
 begin
@@ -41,42 +69,84 @@ begin
 end;
 
 function ReadHeader(hArcData: TArcHandle; var HeaderData: THeaderData
-  ): Integer; cdecl;
+  ): Integer; dcpcall;
 begin
 
 end;
 
 function ReadHeaderEx(hArcData: TArcHandle; var HeaderData: THeaderDataEx
-  ): Integer; cdecl;
+  ): Integer; dcpcall;
 var
   FileSize: Int64;
-  ArchiveEntry: PArchiveEntry;
+  FileName: PAnsiChar;
   Handle: TArchiveHandle absolute hArcData;
 begin
-  Result:= archive_read_next_header(Handle.Archive, ArchiveEntry);
+  Result:= archive_read_next_header(Handle.Archive, Handle.ArchiveEntry);
   if Result = ARCHIVE_EOF then Exit(E_NO_FILES);
-  FileSize:= archive_entry_size(ArchiveEntry);
+  FileSize:= archive_entry_size(Handle.ArchiveEntry);
   HeaderData.UnpSize:= Int64Rec(FileSize).Lo;
   HeaderData.UnpSizeHigh:= Int64Rec(FileSize).Hi;
-  HeaderData.FileAttr:= archive_entry_mode(ArchiveEntry);
-  HeaderData.FileTime:= archive_entry_mtime(ArchiveEntry);
-  HeaderData.FileName:= archive_entry_pathname(ArchiveEntry);
+  HeaderData.FileAttr:= UnixToWinFileAttr(archive_entry_mode(Handle.ArchiveEntry));
+  HeaderData.FileTime:= GetFileTime(archive_entry_mtime(Handle.ArchiveEntry));
+  FileName:= archive_entry_pathname(Handle.ArchiveEntry);
+  GetFileName(FileName, HeaderData);
   Result:= E_SUCCESS;
 end;
 
 function ProcessFile(hArcData: TArcHandle; Operation: Integer; DestPath,
-  DestName: PChar): Integer; cdecl;
+  DestName: PChar): Integer; dcpcall;
 var
+  Size: csize_t;
+  Offset: cint64;
+  FileHandle: PArchive;
+  Buffer: PByte;
   Handle: TArchiveHandle absolute hArcData;
 begin
   Result:= E_SUCCESS;
-  if Operation = PK_SKIP then
-  archive_read_data_skip(Handle.Archive);
+  case Operation of
+    PK_SKIP: archive_read_data_skip(Handle.Archive);
+    PK_EXTRACT:
+      begin
+        FileHandle:= archive_write_disk_new();
+        if FileHandle = nil then Exit(E_NO_MEMORY);
+        WriteLn('1');
+        //archive_write_open_filename(FileHandle, PAnsiChar(String(DestPath) + String(DestName)));
+                WriteLn('2');
+        try
+          archive_entry_set_pathname(Handle.ArchiveEntry, PAnsiChar(String(DestPath) + String(DestName)));
+          archive_write_header(FileHandle, Handle.ArchiveEntry);
+                  WriteLn('3');
+          repeat
+            WriteLn('3/1');
+            Result:= archive_read_data_block(Handle.Archive, @Buffer, @size, @Offset);
+            WriteLn('4');
+            if (Result = ARCHIVE_EOF) then
+            begin
+              WriteLn('4/1');
+              if archive_write_finish_entry(FileHandle) = ARCHIVE_OK then
+                Exit(E_SUCCESS)
+              else begin
+                Exit(E_EWRITE);
+              end;
 
-
+            end;
+            if (Result <> ARCHIVE_OK) then Exit(E_EREAD);
+            Result:= archive_write_data_block(FileHandle, buffer, Size, Offset);
+                        WriteLn('5');
+            if (Result <> ARCHIVE_OK) then Exit(E_EWRITE);
+            ProcessDataProc(PAnsiChar(String(DestPath) + String(DestName)), Offset);
+          until Size = 0;
+        finally
+          WriteLn('5.1 ', archive_error_string(FileHandle));
+          //Offset:= achive_write_free(FileHandle);
+          WriteLn('6');
+        end;
+        if Offset <> ARCHIVE_OK then Result:= E_ECLOSE;
+      end;
+  end;
 end;
 
-function CloseArchive(hArcData: TArcHandle): Integer; cdecl;
+function CloseArchive(hArcData: TArcHandle): Integer; dcpcall;
 var
   Handle: TArchiveHandle absolute hArcData;
 begin
@@ -85,15 +155,15 @@ begin
 end;
 
 procedure SetChangeVolProc(hArcData: TArcHandle; pChangeVolProc: PChangeVolProc
-  ); cdecl;
+  ); dcpcall;
 begin
 
 end;
 
 procedure SetProcessDataProc(hArcData: TArcHandle;
-  pProcessDataProc: TProcessDataProc); cdecl;
+  pProcessDataProc: TProcessDataProc); dcpcall;
 begin
-
+  ProcessDataProc:= pProcessDataProc;
 end;
 
 end.
--- uarchive.pas
+++ uarchive.pas
@@ -5,7 +5,7 @@ unit uArchive;
 interface
 
 uses
-  Classes, SysUtils, CTypes, UnixType;
+  Classes, SysUtils, CTypes;
 
 const
   ARCHIVE_EOF    = 1;	  //* Found end of archive. */
@@ -33,36 +33,43 @@ var
   archive_read_data_skip: function(archive: PArchive): cint; cdecl;
 
   archive_entry_pathname: function(archive_entry: PArchiveEntry): PAnsiChar; cdecl;
-  archive_entry_mtime: function(archive_entry: PArchiveEntry): time_t; cdecl;
-  archive_entry_mode: function(archive_entry: PArchiveEntry): mode_t; cdecl;
+  archive_entry_mtime: function(archive_entry: PArchiveEntry): ptrint; cdecl;
+  archive_entry_mode: function(archive_entry: PArchiveEntry): longword; cdecl;
   archive_entry_size: function(archive_entry: PArchiveEntry): cint64; cdecl;
 
   archive_write_disk_new: function(): PArchive; cdecl;
+  archive_write_open_filename: function(archive: PArchive;
+                                        const file_name: PAnsiChar): cint; cdecl;
   archive_write_header: function(archive: PArchive;
                                  archive_entry: PArchiveEntry): cint; cdecl;
+  archive_write_finish_entry: function(archive: PArchive): cint; cdecl;
   archive_read_data_block: function(archive: PArchive; const buff: PPointer;
                                     size: pcsize_t; offset: pcint64): cint; cdecl;
-  archive_write_data_block: function(archive: PArchive; const buffer: Pointer;
+  archive_write_data_block: function(archive: PArchive; const buffer: PByte;
                                     size: csize_t; offset: cint64): cint64; cdecl;
+  achive_write_free: function(archive: PArchive): cint; cdecl;
   //* Close the file and release most resources. */
   archive_read_close: function(archive: PArchive): cint; cdecl;
   //* Release all resources and destroy the object. */
   //* Note that archive_read_free will call archive_read_close for you. */
   archive_read_free: function(archive: PArchive): cint; cdecl;
 
+  archive_error_string: function(archive: PArchive): PAnsiChar; cdecl;
+  archive_entry_set_pathname: procedure(archive_entry: PArchiveEntry; const file_name: PAnsiChar); cdecl;
+
 
 
 implementation
 
 uses
-  DynLibs;
+  DynLibs, Windows;
 
 var
   libarchive: TLibHandle = NilHandle;
 
 function Load: Boolean;
 begin
-  libarchive:= LoadLibrary('libarchive.so.13');
+  libarchive:= LoadLibrary('libarchive-13.dll');
   Result:= libarchive <> NilHandle;
   if Result then
   begin
@@ -78,8 +85,15 @@ begin
     @archive_entry_size:= GetProcAddress(libarchive, 'archive_entry_size');
     @archive_read_close:= GetProcAddress(libarchive, 'archive_read_close');
     @archive_read_free:= GetProcAddress(libarchive, 'archive_read_free');
-
-
+    @archive_read_data_block:= GetProcAddress(libarchive, 'archive_read_data_block');
+    @archive_write_data_block:= GetProcAddress(libarchive, 'archive_write_data_block');
+    @archive_write_open_filename:= GetProcAddress(libarchive, 'archive_write_open_filename');
+    @archive_write_header:= GetProcAddress(libarchive, 'archive_write_header');
+    @achive_write_free:= GetProcAddress(libarchive, 'achive_write_free');
+    @archive_write_finish_entry:= GetProcAddress(libarchive, 'archive_write_finish_entry');
+    @archive_write_disk_new:= GetProcAddress(libarchive, 'archive_write_disk_new');
+    @archive_error_string:= GetProcAddress(libarchive, 'archive_error_string');
+    @archive_entry_set_pathname:= GetProcAddress(libarchive, 'archive_entry_set_pathname');
   end;
 end;
 
